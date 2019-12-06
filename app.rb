5133fc112b27d5c34eda9e97adf51c9a6fe81530
Zachary Schulz <zjschulz@gmail.com>
2019-12-06 02:23:50 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,7 +2,15 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   
-  get '/' do
+  get '/name' do
+    "Hello, World!"
+  end
+  
+  get '/hometown' do
+    "Hello, World!"
+  end
+  
+  get '/favorite-song' do
     "Hello, World!"
   end
   
